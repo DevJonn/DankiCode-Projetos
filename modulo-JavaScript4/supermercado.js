0154db807eec98bb00793262bbd62b04be5050dc
Jonathan Torres <jonathanlt68@gmail.com>
2023-03-08 13:54:02 -0300

    App Lista de Produtos
    Finalizando Exercicio de lista de produtos usando JavaScript Puro.

--- modulo-JavaScript4/supermercado.js
+++ modulo-JavaScript4/supermercado.js
@@ -2,13 +2,13 @@ var itens = [];
 
 document.querySelector('input[type=submit]')
 .addEventListener('click',()=>{
-    var nomeProduto = document.querySelector('input[name=nome_produto').value;
-    var preçoProduto = document.querySelector('input[name=price').value;
+    var nomeProduto = document.querySelector('input[name=nome_produto');
+    var preçoProduto = document.querySelector('input[name=price');
 
 
     itens.push({
-        nome: nomeProduto,
-        valor: preçoProduto
+        nome: nomeProduto.value,
+        valor: preçoProduto.value 
     });
 
         /*
@@ -19,15 +19,32 @@ document.querySelector('input[type=submit]')
         */
 
         let listaProdutos = document.querySelector('.lista-produtos');
+        let soma = 0;
         listaProdutos.innerHTML = "";
         itens.map(function(val){
+            soma+=parseFloat(val.valor);
+
             listaProdutos.innerHTML+=`
             <div class="lista-produto-single">
             <h3>`+val.nome+`</h3>
-            <h3 class="price-produto"><span>`+valor+`</span></h3>
+            <h3 class="price-produto"><span>R$`+val.valor+`</span></h3>
         </div>
             `;
         })
+        soma = soma.toFixed(2);
+        nomeProduto.value = "";
+        preçoProduto.value = "";
 
+
+        let elementoSoma = document.querySelector('.soma-produto h1');
+        elementoSoma.innerHTML = 'R$'+soma;
 
 });
+
+document.querySelector('button[name=Limpar]')
+.addEventListener('click',()=>{
+    itens = [];
+
+    document.querySelector('.lista-produtos').innerHTML = "";
+    document.querySelector('.soma-produto h1').innerHTML = "R$0";
+})
