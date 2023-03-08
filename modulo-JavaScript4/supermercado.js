44e9a3e22ccab6e285477b042bbef910b0cac953
Jonathan Torres <jonathanlt68@gmail.com>
2023-03-08 11:01:07 -0300

    Update3

--- modulo-JavaScript4/supermercado.js
+++ modulo-JavaScript4/supermercado.js
@@ -3,13 +3,31 @@ var itens = [];
 document.querySelector('input[type=submit]')
 .addEventListener('click',()=>{
     var nomeProduto = document.querySelector('input[name=nome_produto').value;
-    var preçoProduto = document.querySelector('input[name=valor_produto').value;
+    var preçoProduto = document.querySelector('input[name=price').value;
 
-    alert(nomeProduto);
-    alert(preçoProduto);
 
     itens.push({
         nome: nomeProduto,
         valor: preçoProduto
     });
+
+        /*
+        <div class="lista-produto-single">
+            <h3>RedBull</h3>
+            <h3 class="price-produto"><span>$20,00</span></h3>
+        </div>
+        */
+
+        let listaProdutos = document.querySelector('.lista-produtos');
+        listaProdutos.innerHTML = "";
+        itens.map(function(val){
+            listaProdutos.innerHTML+=`
+            <div class="lista-produto-single">
+            <h3>`+val.nome+`</h3>
+            <h3 class="price-produto"><span>`+valor+`</span></h3>
+        </div>
+            `;
+        })
+
+
 });
